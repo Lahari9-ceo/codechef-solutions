63dbac4285fe03fffe36782eab89f458cc651997
Lahari9-ceo <lahari7883@gmail.com>
2026-08-11 20:37:49 +0530

    Added Parity Question and Solution

---
+++ Parity.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int n = sc.nextInt();
+		    if(n%2==0)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		}
+
+	}
+}
